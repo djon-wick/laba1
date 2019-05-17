b13f0dde64a43845f0532015ed7da7340c61575c
djon-wick <50735200+djon-wick@users.noreply.github.com>
2019-05-17 12:48:50 +0300

    Update main.c

--- app/main.c
+++ app/main.c
@@ -4,8 +4,11 @@
 
 int main()
 {
-    int a, b, c;
-    double x1 = 0, x2 = 0;
+    int a;
+    int b;
+    int c;
+    double x1 = 0;
+    double x2 = 0;
     if (scanf("%d %d %d", &a, &b, &c) == 3)
         korenb(a, b, c, &x1, &x2);
     return 0;
